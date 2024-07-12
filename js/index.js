8f3c5552bf807d2d11e5542c3551d467e467231f
Инна Стаценко <innastacenko@Innas-MacBook-Air.local>
2024-07-12 04:20:06 -0700

    Change js

--- js/index.js
+++ js/index.js
@@ -5,22 +5,19 @@ const Themes = {
 };
 
 const root = document.documentElement;
-const button = document.querySelector("#theme-toggle-button");
+const button = document.getElementById("theme-toggle-button");
 
-const toggleTheme = () => {
-    const theme = root.getAttribute(THEME_ATTRIBUTE);
+function toggleTheme() {
+    const currentTheme = root.getAttribute(THEME_ATTRIBUTE);
+    const newTheme = currentTheme === Themes.LIGHT ? Themes.DARK : Themes.LIGHT;
 
-    root.setAttribute(
-        THEME_ATTRIBUTE,
-        theme === Themes.LIGHT ? Themes.DARK : Themes.LIGHT,
-    );
+    root.setAttribute(THEME_ATTRIBUTE, newTheme);
+    button.classList.toggle("header__button_active");
+}
 
-    button.classList.toggle("ds-header__button_active");
-};
-
-const handleButtonClick = (event) => {
+function handleButtonClick(event) {
     event.preventDefault();
     toggleTheme();
-};
+}
 
 button.addEventListener("click", handleButtonClick);
